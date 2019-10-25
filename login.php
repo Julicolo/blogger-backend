41e9ee3117bfb955e4e8851f4d51fb29b003cde2
Julio Schilders <julio.schilders@gmail.com>
2019-10-25 16:42:27 +0200

    Backend now supports user logins

--- login.php
+++ login.php
@@ -4,6 +4,30 @@
     
     $_POST = json_decode(file_get_contents('php://input'), true);
 
-    echo json_encode($_POST);
+    $connection = new mysqli('localhost', 'root', 'root', 'blog');
+    $connection->set_charset('utf8');
 
-    
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $query = $connection->prepare("
+            SELECT users.id, users.username, users.admin
+            FROM users
+            WHERE users.username = ? AND users.password = ?
+        ");
+
+        $query->bind_param(
+          'ss', $_POST['username'], $_POST['password']
+        );
+
+        $query->execute();
+
+        $result = $query->get_result();
+
+        while ($row = $result->fetch_assoc()) {
+            $resultsArray[] = $row;
+        }
+
+        echo json_encode($resultsArray);
+    }
+
+    $connection->close();
+
